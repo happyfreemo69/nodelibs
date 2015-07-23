70e707ad2c6b5aa7c1c9e33c5396a0f49cd5189a
mpech <maxime.pech@freemo.blue>
2015-07-23 10:53:29 +0200

    #1136 allow nodeLibs to exports its libs

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
 var fs = require('fs');
 var exports = module.exports;
-fs.readdir(__dirname+'/lib', function(err, files){
+
+/**
+ * if fname resolves an instance store it in exports[key]
+ * if fname resolves as a constructor, store it in exports[Key]
+ * @param  {string} key   name of module to be exported
+ * @param  {string} fname filename to require
+ */
+function loadModule(key, fname){
+    var module = require('./lib/'+fname);
+    if(module instanceof Function){
+        key = key[0].toUpperCase()+key.substring(1);
+    }
+    exports[key] = module;
+}
+
+function handleFiles(err, files){
     if(err)throw err;
-    files.filter(function(x){
-        return x!='index.js';
-    }).forEach(function(x){
+    var files = [
+        'mdw/contextManager.js',
+        'mdw/reqLogger.js',
+        'validator',
+        'sanitizor',
+        'logger',
+        'tree.js',
+        'urlReplacer.js',
+        'modelPaginator.js',
+        'paramChecker.js',
+        'parseNotifier.js',
+        'queryMerger.js',
+        'destroyer.js',
+        'errorHandler.js',
+        'inheriter.js',
+        'input.js',
+        'agent.js',
+        'bulker.js',
+        'context.js'
+    ]
+    files.forEach(function(x){
         if(x.match(/\.js$/)){
             var exportKey = x.slice(0,-3);
-            exports[exportKey] = require('./lib/'+x);
+            return loadModule(exportKey, x);
+        }
+        var stats = fs.statSync('./lib/'+x);
+        if(stats.isDirectory()){
+            return loadModule(x, x);
         }
     })
-})
+}
+
+
+
+
+
+
+
+
+var files = fs.readdirSync(__dirname+'/lib');
+handleFiles(null, files);
